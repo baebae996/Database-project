33b55c02ca127a805c3591f3b6cd4d948d67512a
baebae996 <2459343217@qq.com>
2020-06-01 19:41:39 +0800

    更新了登录页面

--- static/js/logIn.js
+++ static/js/logIn.js
@@ -1,6 +1,6 @@
 
         
-    //当手机号码输入框获得焦点时，提醒用户输入11位数字
+    //当用户名输入框获得焦点时，提醒用户输入6位数字
     function oFocus_1(){
                
         document.getElementById("remind_1").innerHTML="请输入长度为6位的字母、数字组合";
@@ -15,13 +15,13 @@
     }
 
 
-    //当手机号码输入框失去焦点时，验证是否为空
+    //当用户名输入框失去焦点时，验证是否为空
     function oBlur_1(){
         var oUsername = document.getElementById("username").value;
 
         if(oUsername.length==0)
         {
-            document.getElementById("remind_1").innerHTML="用户名不能为空！";
+            document.getElementById("remind_1").innerHTML="用户名称/商家名称不能为空！";
         }else if(oUsername.length==6)
         {
             document.getElementById("remind_1").innerHTML="";
