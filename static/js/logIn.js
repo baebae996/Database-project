533178ab7ecbcb731bde311239f3cdfd8f9e43d3
baebae996 <2459343217@qq.com>
2020-06-01 10:36:38 +0800

    提交了注册和登录页面的初始版本

---
+++ static/js/logIn.js
@@ -0,0 +1,51 @@
+
+        
+    //当手机号码输入框获得焦点时，提醒用户输入11位数字
+    function oFocus_1(){
+               
+        document.getElementById("remind_1").innerHTML="请输入长度为6位的字母、数字组合";
+        
+    }
+
+    //当密码输入框获得焦点时，提醒用户输入6位字符
+    function oFocus_2(){
+        
+        document.getElementById("remind_2").innerHTML="请输入长度为6位的字母、数字组合";
+    
+    }
+
+
+    //当手机号码输入框失去焦点时，验证是否为空
+    function oBlur_1(){
+        var oUsername = document.getElementById("username").value;
+
+        if(oUsername.length==0)
+        {
+            document.getElementById("remind_1").innerHTML="用户名不能为空！";
+        }else if(oUsername.length==6)
+        {
+            document.getElementById("remind_1").innerHTML="";
+        }else
+        {
+            document.getElementById("remind_1").innerHTML="请输入长度为6位的字母、数字组合";
+        }
+    }
+
+    //当密码输入框失去焦点时，验证是否为空
+    function oBlur_2(){
+        var oPassword = document.getElementById("password").value;
+
+        if(oPassword.length==0)
+        {
+            document.getElementById("remind_2").innerHTML="密码不能为空！";
+        }else if(oPassword.length==6)
+        {
+            document.getElementById("remind_2").innerHTML=""
+        }else
+        {
+            document.getElementById("remind_2").innerHTML="请输入长度为6位的字母、数字组合";
+        }
+    }
+
+
+
